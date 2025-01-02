34331b5e99b1fb97a08489ec8c119abbd681e1a8
vot5kor <vot5kor@gmail.com>
2025-01-02 22:00:42 +0530

    3-variables added

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Declare variables
+P1=ramesh # no space before and after equal
+P2=suresh
+#Substitue the variables  to avoaid static code
+echo "$P1: Hi"
+echo "$P2: hellow"
+echo "$P1: how are you $P2"
+echo "$P2: I am fine $P1"
