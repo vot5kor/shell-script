daa2d456f7a8d684a7c7e072db129a30d0fa407e
vot5kor <vot5kor@gmail.com>
2025-01-05 12:55:52 +0530

    6-data types added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
-    exit 1 #other than 0
+    # exit 1 #other than 0
 fi
 dnf list installed mysql
 
