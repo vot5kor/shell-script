6217902d3cbc2c0c4c942cf95b07992893eca78a
vot5kor <vot5kor@gmail.com>
2025-01-05 11:17:50 +0530

    6-data types added

--- 7-arrays.sh
+++ 7-arrays.sh
@@ -3,7 +3,7 @@
 MOVIES=("pushpa" "rrr" "devara")
 # index starts from 0, size is 3
 
-echo -e "First movie:-Y ${MOVIES[0]}"
+echo "First movie:${MOVIES[0]}"
 echo "Second movie:${MOVIES[1]}"
 echo "Third movie:${MOVIES[2]}"
 
