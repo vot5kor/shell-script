e229ad0c945ca42ca34d6023be6c5b02593aaacb
vot5kor <vot5kor@gmail.com>
2025-01-05 12:58:04 +0530

    10-file added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -9,19 +9,19 @@ then
 fi
 dnf list installed mysql
 
-# if [ $? -ne 0 ]
-# then # not installed
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "Installing MySQL ... FAILURE"
-#         exit 1
-#     else
-#         echo "Installing MySQL ... SUCCESS"
-#     fi
-# else
-#     echo "MySQL is already ... INSTALLED"
-# fi
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
 
 # if [ $? -ne 0 ]
 # then
