5d505c614f998d70a3286551d4f22199d99e872f
vot5kor <vot5kor@gmail.com>
2025-01-05 11:33:33 +0530

    6-data types added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
 
-USERID=$(id -u)
+# USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR:: You must have sudo access to execute this script"
-    exit 1 #other than 0
+# if [ $USERID -ne 0 ]
+# then
+#     echo "ERROR:: You must have sudo access to execute this script"
+#     exit 1 #other than 0
+# fi
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
 fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL ... SUCCESS"
+# fi
+
+# dnf list installed git
+
+# if [ $? -ne 0 ]
+# then
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "Installing Git ... FAILURE"
+#         exit 1
+#     else
+#         echo "Installing Git ... SUCCESS"
+#     fi
+# else
+#     echo "Git is already ... INSTALLED"
+# fi
