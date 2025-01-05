124a40d4cdcdc2c69145f37e67d58347fde0d212
vot5kor <vot5kor@gmail.com>
2025-01-05 11:10:47 +0530

    6-data types added

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "rrr" "devara")
+# index starts from 0, size is 3
+
+echo "First movie: $MOVIES[0]"
+echo "Second movie:$MOVIES[1]"
+echo "Third movie: $MOVIES[2]"
+
+echo "All movies are:$MOVIES[@]"
