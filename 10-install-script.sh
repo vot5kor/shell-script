a5830e811d98b8b6e3df8e36f638973cd9ef571e
vot5kor <vot5kor@gmail.com>
2025-01-08 04:42:39 +0530

    10-file added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -30,7 +30,7 @@ dnf list installed git -y
 if [ $? -ne 0 ]
     then #not Installed
     dnf install git -y
-    if [$? -ne 0 ]
+    if [$? -ne 0]
         then # not Installed
         echo "Installing GIT ... FAILED"
         exit 1
