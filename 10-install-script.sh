8a86cf0c55fc5d37f8d1286e7c6c803e3cad14c1
vot5kor <vot5kor@gmail.com>
2025-01-08 04:30:28 +0530

    10-file added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -9,6 +9,39 @@ then
     exit 1 #other than 0
 fi
 dnf list installed mysql
+if [$? -ne 0]
+then # not installed
+    dnf install mysql -y
+    if [$? -ne 0]
+        then
+        echo "Installing MYSQL ....FAILED"
+        exit 1 
+        else
+        echo "Installing MYSQL ....SUCCESSFULLY"
+    fi
+else
+echo "MYSQL Already... INSTALLED"
+fi.
+
+
+dnf list installed git -y
+
+if [$? -ne 0]
+    then #not Installed
+    dnf install git -y
+    if [$? -ne 0]
+        then # not Installed
+        echo "Installing GIT ... FAILED"
+        exit 1
+        else
+        echo "Installing GIT ... SUCCESSFULLY"
+    fi
+else
+echo "GIT Already ... INSTALLED"
+fi
+
+    
+
 
 # if [ $? -ne 0 ]
 # then # not installed
