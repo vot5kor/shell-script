959ccbf4f9d2c4b3171955f3593a634ab8a4552b
vot5kor <vot5kor@gmail.com>
2025-01-08 05:01:49 +0530

    10-file added

---
+++ 11-functions.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    echo "test: $USERID"
+    exit 1 #other than 0
+fi
+VALIDATE()
+{
+             if [ $1 -ne 0 ]
+                then # not Installed
+                echo "$2 ... FAILED"
+                exit 1
+                else
+                 echo "$2 ... SUCCESSFULLY"
+            fi
+}
+
+dnf list installed git -y
+
+if [ $? -ne 0 ]
+    then #not Installed
+    dnf install git -y
+    VALIDATE $? "Installing GIT"
+    # if [ $? -ne 0 ]
+        # then # not Installed
+        # echo "Installing GIT ... FAILED"
+        # exit 1
+        # else
+        # echo "Installing GIT ... SUCCESSFULLY"
+    # fi
+else
+echo "GIT Already ... INSTALLED"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MYSQL"
+    # if [ $? -ne 0 ]
+        # then
+        # echo "Installing MYSQL ....FAILED"
+        # exit 1 
+        # else
+        # echo "Installing MYSQL ....SUCCESSFULLY"
+    # fi
+    else
+    echo "MYSQL Already... INSTALLED"
+fi
+
+
