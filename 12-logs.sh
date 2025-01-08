6b670a795e58e30340a916a9e5e4dac8eaca5e6c
vot5kor <vot5kor@gmail.com>
2025-01-08 05:53:35 +0530

    10-file added

--- 12-logs.sh
+++ 12-logs.sh
@@ -10,7 +10,7 @@ LOG_FILE="/var/log/shellscript-log"
 FILE_NAME=$(echo $0 | cut -d "." -f1  )
 TIMESTAMP=$(date  +%Y-%m-%d:%H-%M-%S)
 LOG_FILE_NAME="$LOG_FILE/$FILE_NAME-$TIMESTAMP.log"
-echo "test: $LOG_FILE_NAME"
+echo "test: $LOG_FILE_NAME" &>>$LOG_FILE_NAME
 
 VALIDATE()
 {
