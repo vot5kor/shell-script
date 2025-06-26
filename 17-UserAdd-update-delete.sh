4ec877c470e1bddd4945f3363fdc9f7971e6c1ff
vot5kor <vot5kor@gmail.com>
2025-06-26 09:33:55 +0530

    backup files

--- 17-UserAdd-update-delete.sh
+++ 17-UserAdd-update-delete.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
 action="$1"
 username="$2"
-case $action in 
-    "add")
-        useradd "$username" ;;
-    "modify")
-        usermod -s /bin/bash "$username" ;;
-    "delete")
-        userdel "$username" ;;
-     *)
 
- echo "Usage: $0 {add|modify|delete}
-username"
- exit 1 ;;
-Esac
+case "$action" in 
+    add)
+        useradd "$username"
+        ;;
+    modify)
+        usermod -s /bin/bash "$username"
+        ;;
+    delete)
+        userdel "$username"
+        ;;
+    *)
+        echo "Usage: $0 {add|modify|delete} username"
+        exit 1
+        ;;
+esac
